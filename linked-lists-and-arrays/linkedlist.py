8fbc023cb810c6df80b0975e0183da530d6ee44f
Bayesian <singhabhilasha4042@gmail.com>
2021-01-02 15:33:55 +0100

    describes linked list and arrays

---
+++ linked-lists-and-arrays/linkedlist.py
@@ -0,0 +1,28 @@
+class Node: 
+  
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+  
+  
+
+class LinkedList: 
+  
+    
+    def __init__(self): 
+        self.head = None
+  
+  
+
+if __name__=='__main__': 
+  
+    # Start with the empty list 
+    llist = LinkedList() 
+  
+    llist.head = Node(1) 
+    second = Node(2) 
+    third = Node(3) 
+ 
+    llist.head.next = second;
+  
